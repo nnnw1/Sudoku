3cac8f4ab29cb99e3ca81baa1b7dab64f7a0e1df
nnnw1 <gla94@sfu.ca>
2023-06-24 22:33:21 -0700

    define class CScene

--- src/scene.h
+++ src/scene.h
@@ -9,7 +9,42 @@
 
 class CScene
 {
+public:
+    CScene(int index = 3);
+    virtual ~CScene();
 
+    void generate();
+    void show() const;
+
+    bool setCurValue(const int nCurValue, int &nLastValue);
+    bool setPointValue(const point_t &, const int);
+    point_t getCurPoint();
+
+    void eraseRandomGrids(const int count);
+    bool isComplete();
+
+    void play();
+    void save(const char *filename);
+    void load(const char *filename);
+
+    void setMode(KeyMode mode);
+
+private:
+    void init(); //put each grid pointer in the block
+    void setValue(const int);
+    void setValue(const point_t &, const int);
+    void printUnderLine(int line_no = -1) const;
+
+private:
+    KeyMap *keyMap{};
+    int _max_column;
+    point_t _cur_point;
+    CBlock _column_block[9];
+    CBlock _row_block[9];
+    CBlock _xy_block[9];
+    point_value_t _map[81];
+
+    std::vector<CCommand> _vCommand;
 };
 
 #endif
