b24966178651cbf2af9ba3b913ff176de1e19875
nnnw1 <gla94@sfu.ca>
2023-06-25 21:33:30 -0700

    implement test case

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+#include "test.h"
+#include <time.h>
+#include <iostream>
+
+void test_case1()
+{
+    CScene scene;
+    time_t begin, end;
+    time(&begin);
+    scene.generate();
+    time(&end);
+
+    scene.show();
+    std::cout << end - begin << std::endl;
+}
