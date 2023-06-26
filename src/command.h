f5893803112cbbb3b9833a2bb81b77c515e4f19f
nnnw1 <gla94@sfu.ca>
2023-06-25 23:28:20 -0700

    add the files correctly named

---
+++ src/command.h
@@ -0,0 +1,26 @@
+#ifndef _SUDOKU_COMMAND_H_
+#define _SUDOKU_COMMAND_H_
+
+#include <memory>
+#include "common.h"
+
+class CScene;
+class CCommand
+{
+public:
+    CCommand(CScene* pOwner);
+    CCommand(CScene* pOwner, const point_t &point, int preValue, int curValue);
+    CCommand(const CCommand &);
+    ~CCommand();
+
+    bool execute(int nInputValue);
+    void undo();
+
+private:
+    CScene* _pOwner;
+    point_t _stPoint;
+    int _nPreValue;
+    int _nCurValue;
+};
+
+#endif
