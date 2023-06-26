ed271713245df795f64325ff0515d78a29b5c68e
nnnw1 <gla94@sfu.ca>
2023-06-25 23:34:10 -0700

    implement main function

--- src/main.cpp
+++ src/main.cpp
@@ -18,3 +18,35 @@ static void printHelp() {
               << std::endl;
 }
 
+int main(int argc, char **argv)
+{
+#if _TEST_
+    test_case1();
+    getchar();
+#else
+    CScene scene;
+
+    if(argc == 1)
+    {
+        int eraseGridNumber = inputDifficulty();
+        scene.generate();
+        scene.eraseRandomGrids(eraseGridNumber);
+    }
+    else if(argc == 3 && !strcmp(argv[1], "-l"))
+    {
+        //load saved game progress
+        scene.load(argv[2]);
+    }
+    else
+    {
+        printHelp();
+        return 0;
+    }
+
+    scene.setMode(inputKeyMode());
+
+    scene.play();
+#endif
+
+    return 0;
+}
