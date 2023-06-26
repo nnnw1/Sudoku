6aa5c0a10071deab7bcdccf3c89dcb12f09bec73
nnnw1 <gla94@sfu.ca>
2023-06-26 15:17:55 -0700

    finish class CCommand

--- src/command.h
+++ src/command.h
@@ -16,6 +16,13 @@ public:
     bool execute(int nInputValue);
     void undo();
 
+    point_t getPoint() { return _stPoint; }
+    int getPreValue() { return _nPreValue; }
+    int getCurValue() { return _nCurValue; }
+    void setPoint(const point_t &point) { _stPoint = point; }
+    void setPreValue(int preValue) { _nPreValue = preValue; }
+    void setCurValue(int curValue) { _nCurValue = curValue; }
+
 private:
     CScene *_pOwner;
     point_t _stPoint;
